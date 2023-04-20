457c0f383ba6e226851c3f2dde5ca362417defa8
pobby69 <44322089+pobby69@users.noreply.github.com>
2023-04-20 15:49:53 +0100

    formatting & nat gateway

--- main.tf
+++ main.tf
@@ -23,8 +23,10 @@ module "vpc" {
   azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
+  enable_nat_gateway = true
+
   tags = {
-    Terraform = "true"
+    Terraform   = "true"
     Environment = "dev"
   }
 }
@@ -43,13 +45,13 @@ resource "aws_instance" "blog" {
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "4.13.0"
+  
   name    = "blog"
-
   vpc_id  = module.vpc.public_subnets[0]
 
-  ingress_rules = ["http-80-tcp","https-443-tcp"]
+  ingress_rules       = ["http-80-tcp","https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
 
-  egress_rules = ["all-all"]
+  egress_rules       = ["all-all"]
   egress_cidr_blocks = ["0.0.0.0/0"]
 }
